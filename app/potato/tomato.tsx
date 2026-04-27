936bd9006bd4742a2961cdfc7d8bc6c473783875
Jonghwa <whdghk3471@gmail.com>
2026-04-27 10:56:32 +0900

    #1.3 Layout Component

--- app/potato/tomato.tsx
+++ app/potato/tomato.tsx
@@ -1,3 +1,12 @@
+export const links = () => [
+    { rel: "stylesheet", href: "potato.com" },
+]
+
+export const meta = () => [
+    { name: "description", content: "About Us" },
+    {title: "About Us"}
+]
+
 export default function AboutUs() {
     return (
         <div>
