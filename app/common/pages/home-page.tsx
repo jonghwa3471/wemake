78ca2ad2bcbbc4c5755bf713010fc158d20998a7
Jonghwa <whdghk3471@gmail.com>
2026-04-30 14:08:24 +0900

    #3.9 Team Card

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -3,6 +3,7 @@ import { ProductCard } from "~/features/products/components/product-card";
 import { PostCard } from "~/features/community/components/post-card";
 import { IdeaCard } from "~/features/ideas/components/idea-card";
 import { JobCard } from "~/features/jobs/components/job-card";
+import { TeamCard } from "~/features/teams/components/team-card";
 import { Button } from "../components/ui/button";
 
 export const meta: MetaFunction = () => {
@@ -112,6 +113,33 @@ export default function HomePage() {
             positionLocation="Remote"
             salary="$100,000 - $120,000"
             companyHq="San Francisco, CA"
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-4 gap-4">
+        <div>
+          <h2 className="text-5xl leading-tight font-bold tracking-tight">
+            Find a team mate
+          </h2>
+          <p className="text-foreground text-xl font-light">
+            Join a team looking for a new member.
+          </p>
+          <Button variant="link" asChild className="p-0 text-lg">
+            <Link to="/teams">Explore all teams &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 7 }).map((_, index) => (
+          <TeamCard
+            key={`teamId-${index}`}
+            id={`teamId-${index}`}
+            leaderUsername="@j2yonghwa"
+            leaderAvatarUrl="https://github.com/jonghwa3471.png"
+            positions={[
+              "React Developer",
+              "Backend Developer",
+              "Product Manager",
+            ]}
+            projectDescription="a new social media platform."
           />
         ))}
       </div>
