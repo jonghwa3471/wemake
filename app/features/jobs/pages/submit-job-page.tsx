c4497c828eb0611e348dd1eedd1b05c047693868
Jonghwa <whdghk3471@gmail.com>
2026-05-13 13:52:47 +0900

    #3.32 Post a Job Page

--- app/features/jobs/pages/submit-job-page.tsx
+++ app/features/jobs/pages/submit-job-page.tsx
@@ -1,12 +1,14 @@
-import { Form } from "react-router";
-import type { Route } from "./+types/submit-job-page";
 import { PageHero } from "~/common/components/page-hero";
-import { Button } from "~/common/components/ui/button";
+import type { Route } from "./+types/submit-job-page";
+import { Form } from "react-router";
 import InputPair from "~/common/components/input-pair";
+import SelectPair from "~/common/components/select-pair";
+import { JOB_TYPES, LOCATION_TYPES, SALARY_RANGE } from "../constants";
+import { Button } from "~/common/components/ui/button";
 
 export const meta: Route.MetaFunction = () => {
   return [
-    { title: "Submit Job | wemake" },
+    { title: "Post a Job | wemake" },
     { name: "description", content: "Post a job opening" },
   ];
 };
@@ -16,29 +18,144 @@ export default function SubmitJobPage() {
     <div>
       <PageHero
         title="Post a Job"
-        subtitle="Reach candidates on wemake"
+        subtitle="Reach out to the best developers in the world"
       />
-      <Form className="mx-auto grid max-w-2xl gap-6">
-        <InputPair
-          label="Title"
-          description="Role title as candidates will see it"
-          id="title"
-          name="title"
-          type="text"
-          required
-          placeholder="Software Engineer"
-        />
-        <InputPair
-          label="Company"
-          description="Hiring company name"
-          id="company"
-          name="company"
-          type="text"
-          required
-          placeholder="Acme Inc."
-        />
-        <Button type="submit" className="w-fit">
-          Submit
+      <Form className="mx-auto flex max-w-7xl flex-col items-center gap-10">
+        <div className="grid w-full grid-cols-3 gap-10">
+          <InputPair
+            id="position"
+            label="Position"
+            description="(40 characters max)"
+            name="position"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e Senior React Developer"
+          />
+          <InputPair
+            id="overview"
+            label="Overview"
+            description="(400 characters max)"
+            name="overview"
+            maxLength={400}
+            type="text"
+            required
+            placeholder="i.e We are looking for a Senior React Developer"
+            textArea
+          />
+          <InputPair
+            id="responsibilities"
+            label="Responsibilities"
+            description="(400 characters max, comma separated)"
+            name="responsibilities"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e Implement new features, Maintain code quality, etc."
+            textArea
+          />
+          <InputPair
+            id="qualifications"
+            label="Qualifications"
+            description="(400 characters max, comma separated)"
+            name="qualifications"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e 3+ years of experience, Strong TypeScript skills, etc."
+            textArea
+          />
+          <InputPair
+            id="benefits"
+            label="Benefits"
+            description="(400 characters max, comma separated)"
+            name="benefits"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e Flexible working hours, Health insurance, etc."
+            textArea
+          />
+          <InputPair
+            id="skills"
+            label="Skills"
+            description="(400 characters max, comma separated)"
+            name="skills"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e React, TypeScript, etc."
+            textArea
+          />
+          <InputPair
+            id="companyName"
+            label="Company Name"
+            description="(40 characters max)"
+            name="companyName"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e wemake"
+          />
+          <InputPair
+            id="companyName"
+            label="Company Name"
+            description="(40 characters max)"
+            name="companyName"
+            maxLength={40}
+            type="text"
+            required
+            placeholder="i.e wemake"
+          />
+          <InputPair
+            id="companyLogoUrl"
+            label="Company Logo URL"
+            description="(40 characters max)"
+            name="companyLogoUrl"
+            type="url"
+            required
+            placeholder="i.e https://wemake.services/logo.png"
+          />
+          <InputPair
+            id="applyUrl"
+            label="Apply URL"
+            description="(40 characters max)"
+            name="applyUrl"
+            maxLength={40}
+            type="url"
+            required
+            placeholder="i.e https://wemake.services/apply"
+          />
+          <SelectPair
+            label="Job Type"
+            description="Select the type of job"
+            name="jobType"
+            required
+            placeholder="Select the type of job"
+            options={JOB_TYPES.map((type) => type)}
+          />
+          <SelectPair
+            label="Job Location"
+            description="Select the location of the job"
+            name="jobLocation"
+            required
+            placeholder="Select the location of the job"
+            options={LOCATION_TYPES.map((location) => location)}
+          />
+          <SelectPair
+            label="Salary Range"
+            description="Select the salary range of the job"
+            name="salaryRange"
+            required
+            placeholder="Select the salary range of the job"
+            options={SALARY_RANGE.map((salary) => ({
+              label: salary,
+              value: salary,
+            }))}
+          />
+        </div>
+        <Button type="submit" className="w-full max-w-sm" size={"lg"}>
+          Post job for $100
         </Button>
       </Form>
     </div>
