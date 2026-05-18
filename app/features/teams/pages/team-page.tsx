9523d49a96dbc212ed4169e49fc27cac065b7570
Jonghwa <whdghk3471@gmail.com>
2026-05-18 16:56:25 +0900

    #3.39 Team Page

--- app/features/teams/pages/team-page.tsx
+++ app/features/teams/pages/team-page.tsx
@@ -1,5 +1,5 @@
-import type { Route } from "./+types/team-page";
 import { PageHero } from "~/common/components/page-hero";
+import type { Route } from "./+types/team-page";
 import {
   Avatar,
   AvatarFallback,
@@ -7,52 +7,114 @@ import {
 } from "~/common/components/ui/avatar";
 import { Badge } from "~/common/components/ui/badge";
 import { Button } from "~/common/components/ui/button";
+import { Form } from "react-router";
+import InputPair from "~/common/components/input-pair";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/common/components/ui/card";
 
 export const meta: Route.MetaFunction = () => {
   return [{ title: "Team Details | wemake" }];
 };
 
 export default function TeamPage() {
-  const positions = [
-    "React Developer",
-    "Backend Developer",
-    "Product Manager",
-  ];
-
   return (
     <div className="space-y-20">
-      <PageHero title="Team" subtitle="Build something great together" />
-      <div className="mx-auto flex max-w-3xl flex-col gap-10">
-        <div className="flex items-center gap-4">
-          <Avatar className="size-14">
-            <AvatarFallback>J</AvatarFallback>
-            <AvatarImage src="https://github.com/jonghwa3471.png" />
-          </Avatar>
-          <div>
-            <h2 className="text-2xl font-bold">@j2yonghwa</h2>
-            <p className="text-muted-foreground">Team leader</p>
+      <PageHero title="Join lynn's team" />
+      <div className="grid grid-cols-6 items-start gap-40">
+        <div className="col-span-4 grid grid-cols-4 gap-5">
+          {[
+            {
+              title: "Product name",
+              value: "Doggy Social",
+            },
+            {
+              title: "Stage",
+              value: "MVP",
+            },
+            {
+              title: "Team size",
+              value: 3,
+            },
+            {
+              title: "Available equity",
+              value: 50,
+            },
+          ].map((item, index) => (
+            <Card key={index}>
+              <CardHeader>
+                <CardTitle className="text-muted-foreground text-sm font-medium">
+                  {item.title}
+                </CardTitle>
+                <CardContent className="p-0 text-2xl font-bold">
+                  <p>{item.value}</p>
+                </CardContent>
+              </CardHeader>
+            </Card>
+          ))}
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-muted-foreground text-sm font-medium">
+                Looking for
+              </CardTitle>
+              <CardContent className="p-0 text-2xl font-bold">
+                <ul className="list-inside list-disc text-lg">
+                  {[
+                    "React Developer",
+                    "Backend Developer",
+                    "Product Manager",
+                    "UI/UX Designer",
+                  ].map((item) => (
+                    <li key={item}>{item}</li>
+                  ))}
+                </ul>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card className="col-span-2">
+            <CardHeader>
+              <CardTitle className="text-muted-foreground text-sm font-medium">
+                Idea description
+              </CardTitle>
+              <CardContent className="p-0 text-xl font-medium">
+                <p>
+                  Doggy Social is a social media platform for dogs. It allows
+                  dogs to connect with each other and share their experiences.
+                </p>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+        <aside className="col-span-2 space-y-5 rounded-lg border p-6 shadow-sm">
+          <div className="flex gap-5">
+            <Avatar className="size-14">
+              <AvatarFallback>N</AvatarFallback>
+              <AvatarImage src="https://github.com/inthetiger.png" />
+            </Avatar>
+            <div className="flex flex-col">
+              <h4 className="text-lg font-medium">Lynn</h4>
+              <Badge variant={"secondary"}>Entrepreneur</Badge>
+            </div>
           </div>
-        </div>
-        <div className="space-y-2">
-          <h3 className="text-xl font-bold">Looking for</h3>
-          <div className="flex flex-wrap gap-2">
-            {positions.map((role) => (
-              <Badge key={role} className="text-base">
-                {role}
-              </Badge>
-            ))}
-          </div>
-        </div>
-        <div className="space-y-2">
-          <h3 className="text-xl font-bold">Project</h3>
-          <p className="text-lg">
-            We are building a new social media platform focused on connecting
-            makers and sharing product launches.
-          </p>
-        </div>
-        <Button size="lg" className="self-center">
-          Join team &rarr;
-        </Button>
+          <Form className="space-y-5">
+            <InputPair
+              label="Introduce yourself"
+              description="Tell us about yourself"
+              name="introduction"
+              type="text"
+              id="introduction"
+              required
+              textArea
+              placeholder="i.e I'm a React Developer with 3 years of experience"
+            />
+          </Form>
+          <Button className="w-full" type="submit">
+            Get in touch
+          </Button>
+        </aside>
       </div>
     </div>
   );
