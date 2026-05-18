7036d5e759053e93aec08c8ae134a1d0137157ef
Jonghwa <whdghk3471@gmail.com>
2026-05-18 11:07:00 +0900

    #3.37 Post Replies

---
+++ app/features/community/components/reply.tsx
@@ -0,0 +1,83 @@
+import { Form, Link } from "react-router";
+import { DotIcon, MessageCircleIcon } from "lucide-react";
+import { Button } from "~/common/components/ui/button";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/common/components/ui/avatar";
+import { useState } from "react";
+import { Textarea } from "~/common/components/ui/textarea";
+
+interface ReplyProps {
+  avatarUrl: string;
+  username: string;
+  timestamp: string;
+  content: string;
+  topLevel: boolean;
+}
+
+export function Reply({
+  avatarUrl,
+  username,
+  timestamp,
+  content,
+  topLevel,
+}: ReplyProps) {
+  const [replying, setReplying] = useState(false);
+  const toggleReplying = () => setReplying((prev) => !prev);
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex w-2/3 items-start gap-5">
+        <Avatar className="size-14">
+          <AvatarFallback>{username[0]}</AvatarFallback>
+          <AvatarImage src={avatarUrl} />
+        </Avatar>
+        <div className="flex flex-col items-start gap-2">
+          <div className="flex items-center gap-2">
+            <Link to={`/users/@${username}`}>
+              <h4 className="font-medium">{username}</h4>
+            </Link>
+            <DotIcon className="size-5" />
+            <span className="text-muted-foreground text-xs">{timestamp}</span>
+          </div>
+          <p className="text-muted-foreground">{content}</p>
+          <Button
+            variant={"ghost"}
+            className="self-end"
+            onClick={toggleReplying}
+          >
+            <MessageCircleIcon className="size-4" /> Reply
+          </Button>
+        </div>
+      </div>
+      {replying && (
+        <Form className="flex w-3/4 items-start gap-5">
+          <Avatar className="size-14">
+            <AvatarFallback>N</AvatarFallback>
+            <AvatarImage src="https://github.com/jonghwa3471.png" />
+          </Avatar>
+          <div className="flex w-full flex-col items-end gap-5">
+            <Textarea
+              placeholder="Write a reply"
+              className="w-full resize-none"
+              rows={5}
+            />
+            <Button>Reply</Button>
+          </div>
+        </Form>
+      )}
+      {topLevel && (
+        <div className="w-full pl-20">
+          <Reply
+            avatarUrl="https://github.com/jonghwa3471.png"
+            username="j2yonghwa"
+            timestamp="12 hours ago"
+            content="I've been using Todoist for a while now, and it's really great. It's simple, easy to use, and has a lot of features."
+            topLevel={false}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
--- app/features/community/pages/post-page.tsx
+++ app/features/community/pages/post-page.tsx
@@ -15,6 +15,8 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "~/common/components/ui/avatar";
+import { Badge } from "~/common/components/ui/badge";
+import { Reply } from "~/features/community/components/reply";
 
 export const meta: Route.MetaFunction = ({ params }) => {
   return [{ title: `${params.postId} | wemake` }];
@@ -22,74 +24,106 @@ export const meta: Route.MetaFunction = ({ params }) => {
 
 export default function PostPage({}: Route.ComponentProps) {
   return (
-    <div className="grid grid-cols-6 items-start gap-40">
-      <div className="col-span-4 space-y-10">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link to={`/community`}>Community</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link to={`/community?topic=productivity`}>Productivity</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator />
-            <BreadcrumbItem>
-              <BreadcrumbLink asChild>
-                <Link to={`/community/postId`}>
-                  What is the best productivity tool?
-                </Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
-        <div className="flex w-full items-start gap-10">
-          <Button variant="outline" className="flex h-14 flex-col">
-            <ChevronUpIcon className="size-4 shrink-0" />
-            <span>{10}</span>
-          </Button>
-          <div className="space-y-20">
-            <div className="space-y-2">
-              <h2 className="text-3xl font-bold">
+    <div className="space-y-10">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to={`/community`}>Community</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to={`/community?topic=productivity`}>Productivity</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to={`/community/postId`}>
                 What is the best productivity tool?
-              </h2>
-              <div className="text-muted-foreground flex items-center gap-2 text-sm">
-                <span>@nico</span>
-                <DotIcon className="size-5" />
-                <span>12 hours ago</span>
-                <DotIcon className="size-5" />
-                <span>10 replies</span>
+              </Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="grid grid-cols-6 items-start gap-40">
+        <div className="col-span-4 space-y-10">
+          <div className="flex w-full items-start gap-10">
+            <Button variant="outline" className="flex h-14 flex-col">
+              <ChevronUpIcon className="size-4 shrink-0" />
+              <span>{10}</span>
+            </Button>
+            <div className="space-y-20">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold">
+                  What is the best productivity tool?
+                </h2>
+                <div className="text-muted-foreground flex items-center gap-2 text-sm">
+                  <span>@nico</span>
+                  <DotIcon className="size-5" />
+                  <span>12 hours ago</span>
+                  <DotIcon className="size-5" />
+                  <span>10 replies</span>
+                </div>
+                <p className="text-muted-foreground max-w-3/4">
+                  Hello, I'm looking for a productivity tool that can help me
+                  manage my tasks and projects. Any recommendations? I have
+                  tried Notion, but it's not what I'm looking for. I dream of a
+                  tool that can help me manage my tasks and projects. Any
+                  recommendations?
+                </p>
               </div>
-              <p className="text-muted-foreground max-w-3/4">
-                Hello, I'm looking for a productivity tool that can help me
-                manage my tasks and projects. Any recommendations? I have tried
-                Notion, but it's not what I'm looking for. I dream of a tool
-                that can help me manage my tasks and projects. Any
-                recommendations?
-              </p>
+              <Form className="flex w-3/4 items-start gap-5">
+                <Avatar className="size-14">
+                  <AvatarFallback>N</AvatarFallback>
+                  <AvatarImage src="https://github.com/jonghwa3471.png" />
+                </Avatar>
+                <div className="flex w-full flex-col items-end gap-5">
+                  <Textarea
+                    placeholder="Write a reply"
+                    className="w-full resize-none"
+                    rows={5}
+                  />
+                  <Button>Reply</Button>
+                </div>
+              </Form>
+              <div className="space-y-10">
+                <h4 className="font-semibold">10 Replies</h4>
+                <div className="flex flex-col gap-5">
+                  <Reply
+                    avatarUrl="https://github.com/jonghwa3471.png"
+                    username="j2yonghwa"
+                    timestamp="12 hours ago"
+                    content="I've been using Todoist for a while now, and it's really great. It's simple, easy to use, and has a lot of features."
+                    topLevel
+                  />
+                </div>
+              </div>
             </div>
-            <Form className="flex w-3/4 items-start gap-5">
-              <Avatar className="size-14">
-                <AvatarFallback>N</AvatarFallback>
-                <AvatarImage src="https://github.com/jonghwa3471.png" />
-              </Avatar>
-              <div className="flex w-full flex-col items-end gap-5">
-                <Textarea
-                  placeholder="Write a reply"
-                  className="w-full resize-none"
-                  rows={5}
-                />
-                <Button>Reply</Button>
-              </div>
-            </Form>
           </div>
         </div>
+        <aside className="col-span-2 space-y-5 rounded-lg border p-6 shadow-sm">
+          <div className="flex gap-5">
+            <Avatar className="size-14">
+              <AvatarFallback>N</AvatarFallback>
+              <AvatarImage src="https://github.com/microsoft.png" />
+            </Avatar>
+            <div className="flex flex-col">
+              <h4 className="text-lg font-medium">Nicolas</h4>
+              <Badge variant={"secondary"}>Entrepreneur</Badge>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2 text-sm">
+            <span>🎂 Joined 3 months ago</span>
+            <span>🚀 Launched 10 products</span>
+          </div>
+          <Button variant={"outline"} className="w-full">
+            Follow
+          </Button>
+        </aside>
       </div>
-      <aside className="col-span-2"></aside>
     </div>
   );
 }
