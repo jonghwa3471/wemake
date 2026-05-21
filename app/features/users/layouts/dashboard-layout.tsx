9c1a9b82f5b6703a1aceb3dafbcffddbb709c6a2
Jonghwa <whdghk3471@gmail.com>
2026-05-21 11:36:05 +0900

    #3.45 Dashboard Page

---
+++ app/features/users/layouts/dashboard-layout.tsx
@@ -0,0 +1,67 @@
+import { Link, Outlet, useLocation } from "react-router";
+import { MessagesCard } from "~/features/users/components/messages-card";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "~/common/components/ui/sidebar";
+import { HomeIcon, RocketIcon, SparklesIcon } from "lucide-react";
+
+export default function DashboardLayout() {
+  const location = useLocation();
+  return (
+    <SidebarProvider className="flex min-h-full">
+      <Sidebar className="pt-16" variant="floating">
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  asChild
+                  isActive={location.pathname === "/my/dashboard"}
+                >
+                  <Link to={`/my/dashboard`}>
+                    <HomeIcon className="size-4" />
+                    <span>Home</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton
+                  asChild
+                  isActive={location.pathname === "/my/dashboard/ideas"}
+                >
+                  <Link to={`/my/dashboard/ideas`}>
+                    <SparklesIcon className="size-4" />
+                    <span>Ideas</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+          <SidebarGroup>
+            <SidebarGroupLabel>Product Analytics</SidebarGroupLabel>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton asChild>
+                  <Link to={`/my/dashboard/products/1`}>
+                    <RocketIcon className="size-4" />
+                    <span>Product 1</span>
+                  </Link>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+      <div className="flex-1 p-1">
+        <Outlet />
+      </div>
+    </SidebarProvider>
+  );
+}
--- app/features/users/pages/dashboard-ideas-page.tsx
+++ app/features/users/pages/dashboard-ideas-page.tsx
@@ -1,3 +1,21 @@
+import { IdeaCard } from "~/features/ideas/components/idea-card";
+
 export default function DashboardIdeasPage() {
-  return <div>Dashboard ideas</div>;
+  return (
+    <div className="h-full space-y-5">
+      <h1 className="mb-6 text-2xl font-semibold">Claimed Ideas</h1>
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-6">
+        {Array.from({ length: 5 }).map((_, index) => (
+          <IdeaCard
+            key={`ideaId-${index}`}
+            id={`ideaId-${index}`}
+            title="A startup that creates an AI-powered generated personal trainer, delivering customized fitness recommendations and tracking of progress using a mobile app to track workouts and progress as well as a website to manage the business."
+            viewsCount={123}
+            postedAt="12 hours ago"
+            likesCount={12}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
