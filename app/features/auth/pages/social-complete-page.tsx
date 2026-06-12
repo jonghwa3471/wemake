73fafb4ae87e50abc16a5c79217cab77152d434e
Jonghwa <whdghk3471@gmail.com>
2026-06-12 16:48:43 +0900

    #7.7 Social Login

--- app/features/auth/pages/social-complete-page.tsx
+++ app/features/auth/pages/social-complete-page.tsx
@@ -1,9 +1,24 @@
+import z from "zod";
 import type { Route } from "./+types/social-complete-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
 
-export const meta: Route.MetaFunction = () => {
-  return [{ title: "Social sign-in complete | wemake" }];
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+  if (!code) {
+    return redirect("/auth/login");
+  }
+  const { client, headers } = makeSSRClient(request);
+  const { error } = await client.auth.exchangeCodeForSession(code);
+  if (error) throw error;
+  return redirect("/", { headers });
 };
-
-export default function SocialCompletePage() {
-  return <div>Social complete</div>;
-}
--- app/features/auth/pages/social-start-page.tsx
+++ app/features/auth/pages/social-start-page.tsx
@@ -1,9 +1,31 @@
+import z from "zod";
 import type { Route } from "./+types/social-start-page";
+import { redirect } from "react-router";
+import { makeSSRClient } from "~/supa-client";
 
-export const meta: Route.MetaFunction = () => {
-  return [{ title: "Social sign-in | wemake" }];
+const paramsSchema = z.object({
+  provider: z.enum(["github", "kakao"]),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const { success, data } = paramsSchema.safeParse(params);
+  if (!success) {
+    return redirect("/auth/login");
+  }
+  const { provider } = data;
+  const redirectTo = `http://localhost:5173/auth/social/${provider}/complete`;
+  const { client, headers } = makeSSRClient(request);
+  const {
+    data: { url },
+    error,
+  } = await client.auth.signInWithOAuth({
+    provider,
+    options: {
+      redirectTo,
+    },
+  });
+  if (url) {
+    return redirect(url, { headers });
+  }
+  if (error) throw error;
 };
-
-export default function SocialStartPage() {
-  return <div>Social start</div>;
-}
