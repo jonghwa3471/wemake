9173e0901928f6d7f9a47b349f9727d2c396bfd6
Jonghwa <whdghk3471@gmail.com>
2026-07-23 11:59:24 +0900

    edit: add classname dark

--- app/root.tsx
+++ app/root.tsx
@@ -34,7 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   Settings.defaultLocale = "ko";
   Settings.defaultZone = "Asia/Seoul";
   return (
-    <html lang="en" className="">
+    <html lang="en" className="dark">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
